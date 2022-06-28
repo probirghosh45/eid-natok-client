8c6641b67a06ef9e7b8b18e242b3b44a437ebde0
probir ghosh <probirghosh@outlook.com>
2022-06-29 03:41:52 +0600

    flowbite initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,12 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
