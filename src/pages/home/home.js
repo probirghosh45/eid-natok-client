b271ac6aaf424998bbfd29c5722ba93f5f466f3f
probir ghosh <probirghosh@outlook.com>
2022-06-29 04:15:41 +0600

    navbar added

---
+++ src/pages/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    );
+};
+
+export default Home;
