19e7ba5eb585b7baa40694e0c8576f0c5a129526
probir ghosh <probirghosh@outlook.com>
2022-07-05 13:34:47 +0600

    eid natok starter

--- src/Components/ManageNatokDetails.js
+++ src/Components/ManageNatokDetails.js
@@ -67,7 +67,7 @@ const ManageNatokDetails = ({ natok }) => {
                               >
                                 Director Name
                               </label>
-                              <input
+                              <input 
                                 class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                 id="grid-last-name"
                                 type="text"
